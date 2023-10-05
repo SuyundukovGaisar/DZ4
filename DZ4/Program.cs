05b1b4fadb3313e94cb659b285b962547ae89f84
user <user@WIN-EL4IOEQBAEK>
2023-10-05 21:01:38 +0300

    Добавьте файлы проекта.

---
+++ DZ4/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1(Вывести на экран массив из 20 случайных чисел и ввести два числа из массива,которые нужно поменять местами)");
+            int[] numbers = new int[20];
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 100);
+            }
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Введите номер первого числа: ");
+            int firstIndex = int.Parse(Console.ReadLine()) - 1;
+            Console.Write("Введите номер второго числа: ");
+            int secondIndex = int.Parse(Console.ReadLine()) - 1;
+            if (firstIndex < 0 || firstIndex >= numbers.Length || secondIndex < 0 || secondIndex >= numbers.Length)
+            {
+                Console.WriteLine("Введенные индексы являются недопустимыми.");
+                return;
+            }
+            int temp = numbers[firstIndex];
+            numbers[firstIndex] = numbers[secondIndex];
+            numbers[secondIndex] = temp;
+            Console.WriteLine("Получившийся массив:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 2(Написать метод, где в качества аргумента будет передан массив.Вывести сумму,(ref) произведение,(out) среднее арифметическое элементов массива)");
+            Console.WriteLine("Сумма элементов: " + CalculateSum(1, 2, 3, 4, 5));
+            Console.WriteLine("Произведение элементов: " + CalculateProduct(out int product, 1, 2, 3, 4, 5));
+            Console.WriteLine("Среднее арифметическое: " + CalculateAverage(1, 2, 3, 4, 5));
+
+            Console.WriteLine("Задание 3(Если числа от 0 до 9, то необходимо в консоли нарисовать изображение цифры в виде символа, если ввести exit или закрыть программа закроется)");
+            bool exit = false;
+            while (!exit)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Введите число от 0 до 9 (для выхода введите 'exit'): ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit" || input.ToLower() == "закрыть")
+                {
+                    exit = true;
+                    continue;
+                }
+
+                int number;
+                if (!int.TryParse(input, out number))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ошибка! Введено не число!");
+                    System.Threading.Thread.Sleep(3000);
+                    continue;
+                }
+
+                if (number < 0 || number > 9)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ошибка! Введено число вне диапазона!");
+                    System.Threading.Thread.Sleep(3000);
+                    continue;
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Изображение цифры {0}:", number);
+                DrawNumber(number);
+                Console.WriteLine();
+
+                Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                Console.ReadKey();
+            }
+
+        }
+        static int CalculateSum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+        static int CalculateProduct(out int product, params int[] numbers)
+        {
+            product = 1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                product *= numbers[i];
+            }
+            return product;
+        }
+        static double CalculateAverage(params int[] numbers)
+        {
+            int sum = CalculateSum(numbers);
+            double average = (double)sum / numbers.Length;
+            return average;
+        }
+        static void DrawNumber(int number)
+        {
+            string[] digits = new string[]
+            {
+            "  ###  \n #   # \n #   # \n #   # \n  ###  ", // 0
+            "   #   \n   #   \n   #   \n   #   \n   #   ", // 1
+            "  ###  \n     # \n  ###  \n #     \n  ###  ", // 2
+            "  ###  \n     # \n  ###  \n     # \n  ###  ", // 3
+            " #   # \n #   # \n  ###  \n     # \n     # ", // 4
+            "  ###  \n #     \n  ###  \n     # \n  ###  ", // 5
+            "  ###  \n #     \n  ###  \n #   # \n  ###  ", // 6
+            "  ###  \n     # \n    #  \n   #   \n   #   ", // 7
+            "  ###  \n #   # \n  ###  \n #   # \n  ###  ", // 8
+            "  ###  \n #   # \n  ###  \n     # \n  ###  "  // 9
+            };
+
+            Console.WriteLine(digits[number]);
+        }
+    }
+}
---
+++ Tumakov_Lab5/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumakov_Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 5.1 (Написать метод, возвращающий наибольшее из двух чисел)");
+            try
+            {
+                Console.WriteLine("Введите 1 целое число (после ввода нажмите enter): ");
+                bool a = int.TryParse(Console.ReadLine(), out int num1);
+                Console.WriteLine("Введите 2 целое число (после ввода нажмите enter): ");
+                bool b = int.TryParse(Console.ReadLine(), out int num2);
+                Console.WriteLine($"Максимальное значение равно: {MaxValue(num1, num2)}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Упражнение 5.2 (Написать метод, который меняет местами значения двух передаваемых параметров)");
+            Console.WriteLine("Введите 1 параметр (после ввода нажмите enter): ");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Введите 2 параметр (после ввода нажмите enter): ");
+            string number2 = Console.ReadLine();
+            Swap(ref number1, ref number2);
+            Console.WriteLine($"Результат: {number1}, {number2}");
+
+            Console.WriteLine("Упражнение 5.3 (Написать метод вычисления факториала числа)");
+            try
+            {
+                Console.WriteLine("Введите число, факториал которого нужно вычислить(после ввода нажмите enter):");
+                bool d = int.TryParse(Console.ReadLine(), out int num);
+                long fact = 0;
+                if (ComputeFactorial(num, out fact))
+                {
+                    Console.WriteLine($"Факториал от {num} это {fact}");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Вычислить факториал данного числа не удалось в связи с переполнением");
+            }
+
+            Console.WriteLine("Упражнение 5.4 (Написать рекурсивный метод вычисления факториала числа)");
+            Console.WriteLine("Введите число, факториал которого нужно вычислить(после ввода нажмите enter):");
+            bool c = int.TryParse(Console.ReadLine(), out int value);
+            Console.WriteLine("Факториал = " + Factorial(value));
+
+            Console.WriteLine("Домашнее задание 5.1 (Написать метод, который вычисляет НОД двух натуральных чисел)");
+            try
+            {
+                Console.WriteLine("Введите 1 натуральное число (после ввода нажмите enter): ");
+                int m = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите 2 натуральное число (после ввода нажмите enter): ");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(Evklid(m, n));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+          
+            Console.WriteLine("Домашнее задание 5.2 (Написать рекурсивный метод, вычисляющий значение n-го числа ряда Фибоначчи)");
+            Console.WriteLine("Введите число N(после ввода нажмите enter): ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Результат равен: {Fibonachi(N)}");
+        }
+        public static int MaxValue(int num1, int num2)
+        {
+            return Math.Max(num1, num2);
+        }
+        static void Swap(ref string number1, ref string number2)
+        {
+            string temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+        public static bool ComputeFactorial(int num, out long fact)
+        {
+            checked
+            {
+                fact = 1;
+                for (int i = 2; i <= num; i++)
+                {
+                    fact *= i;
+                }
+                return true;
+            }
+        }
+        static int Factorial(int value)
+        {
+            if (value == 0)
+            {
+                return 1;
+            }
+            return value * Factorial(value - 1);
+        }
+        static int Evklid(int m, int n)
+        {
+            while (m != n)
+            {
+                if (m > n)
+                {
+                    m = m - n;
+                }
+                else
+                {
+                    n = n - m;
+                }
+            }
+
+            return n;
+        }
+        static int Fibonachi(int N)
+        {
+            if (N == 0)
+            {
+                return 0;
+            }
+            else if (N == 1 || N == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonachi(N - 1) + Fibonachi(N - 2);
+            }
+        }
+
+    }
+}
